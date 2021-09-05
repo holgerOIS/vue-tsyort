7b9952677349dfcff6b71b8806e0e1b0ad83d84a
holgerOIS <ois@gamboeck.net>
2021-09-05 11:52:11 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+    <HelloWorld :msg="ttt"/>
+    <div v-for="t in ttt">
+      <Test :msg="t"/>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Test from './components/test.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld, Test
+  },
+  data(){
+    return {
+      ttt:["Holger", "Vera", "Adrian", "JULIA"]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
